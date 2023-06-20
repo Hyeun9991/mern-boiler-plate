0b4b17aa76b007131c91c1afe50468d0e22aca88
Hyeun9991 <hyeun9991@gmail.com>
2023-06-20 18:57:09 +0900

    7nd commit: Add Tailwind CSS setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './global.css';
+import 'tailwindcss/tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
