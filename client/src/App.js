ef15bf866ea058f23c8abb20073dbf7c5a058aba
Hyeun9991 <hyeun9991@gmail.com>
2023-06-21 07:41:45 +0900

    9nd commit: React Router Dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+
 function App() {
   return (
-    <div>
-      <h1>welcome</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
