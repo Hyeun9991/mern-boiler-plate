a9be540b040ce288812416585200a814635d866a
Hyeun9991 <hyeun9991@gmail.com>
2023-06-20 13:20:45 +0900

    Set up Express app and connect to MongoDB

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import userRoutes from './routes/users.js';
+
+// 설정
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+// Routes 설정
+app.use('/api/users', userRoutes);
+
+// Mongoose 설정
+const PORT = process.env.PORT || 8123;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
